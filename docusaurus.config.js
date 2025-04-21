a195e82458004e70f5a17403e8161eed403f9690
彬少 <50693793+zuqiuxie@users.noreply.github.com>
2025-04-22 00:25:15 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -197,6 +197,9 @@ const config = {
             href: "https://zazhi.zhihu.blog/",
             label: "电子书下载",
           },
+          document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KnEkUhhnL1LAUT8U\",ck:\"KnEkUhhnL1LAUT8U\"});</script>")
+
+
 
           /*
           {
